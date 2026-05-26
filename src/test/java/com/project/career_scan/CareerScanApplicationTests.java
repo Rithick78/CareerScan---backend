57487c473a9cdb14e95e96343dc26110a2044282
rithickjackson8 <rithickjackson8@gmail.com>
2026-05-26 17:23:34 +0530

    git commit -m "feat: project init, User entity + repo, BCrypt config, Strutured folder"

---
+++ src/test/java/com/project/career_scan/CareerScanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.career_scan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerScanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
